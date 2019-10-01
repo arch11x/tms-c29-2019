59a4a5cc7a0fac29ce40d7543115be5ea54ed710
Alexander Fomin <rezviybelorus@gmail.com>
2019-10-01 14:47:37 +0300

    lec_1

--- src/tms/c29/TestMain.java
+++ src/tms/c29/TestMain.java
@@ -4,5 +4,16 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println(8<<1);
+
+        boolean b = true;
+        double d = 100;
+
+        int x = (int) d;
+
+        char c = '\u039A';
+        System.out.println(c);
+
+        c = 33;
+        System.out.println(c);
     }
 }
