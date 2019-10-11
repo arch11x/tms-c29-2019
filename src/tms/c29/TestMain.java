f116a6a75dfd2bfe71d7f362b20fa8afa02dc58e
Alexander Fomin <rezviybelorus@gmail.com>
2019-10-11 17:32:32 +0300

    lec_3

--- src/tms/c29/TestMain.java
+++ src/tms/c29/TestMain.java
@@ -1,46 +1,53 @@
 package tms.c29;
 
-import java.util.Arrays;
-
 public class TestMain {
     public static void main(String... args) {
-        int[] ints = {2, 3, 0, 1, 6, 6, 6, 6, 6, 6, 6, 6};
+//        int[] ints = {2, 3, 0, 1, 6, 6, 6, 6, 6, 6, 6, 6};
+//
+//        int iterCounter = 0;
+//        for (int i = 0; i < ints.length; i++) {
+//            for (int j = 1; j < ints.length - i; j++) {
+//                iterCounter++;
+//                if (ints[j] < ints[j - 1]) {
+//                    int temp = ints[j];
+//                    ints[j] = ints[j - 1];
+//                    ints[j - 1] = temp;
+//                }
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(ints));
+//        System.out.println("counter: " + iterCounter);
+//
+//        ints = new int[]{2, 3, 0, 1};
+//
+//        iterCounter = 0;
+//        boolean needIteration = true;
+//        while (needIteration) {
+//            needIteration = false;
+//            for (int i = 1; i < ints.length; i++) {
+//                iterCounter++;
+//                if (ints[i] < ints[i - 1]) {
+//                    int temp = ints[i];
+//                    ints[i] = ints[i - 1];
+//                    ints[i - 1] = temp;
+//
+//                    needIteration = true;
+//                }
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(ints));
+//        System.out.println("counter: " + iterCounter);
 
-        int iterCounter = 0;
+        int[][] ints = new int[3][2];
+
         for (int i = 0; i < ints.length; i++) {
-            for (int j = 1; j < ints.length - i; j++) {
-                iterCounter++;
-                if (ints[j] < ints[j - 1]) {
-                    int temp = ints[j];
-                    ints[j] = ints[j - 1];
-                    ints[j - 1] = temp;
-                }
+            for (int j = 0; j < ints[j].length; j++) {
+                System.out.print(ints[i][j] + " ");
             }
+            System.out.println();
         }
-
-        System.out.println(Arrays.toString(ints));
-        System.out.println("counter: " + iterCounter);
-
-        ints = new int[]{2, 3, 0, 1};
-
-        iterCounter = 0;
-        boolean needIteration = true;
-        while (needIteration) {
-            needIteration = false;
-            for (int i = 1; i < ints.length; i++) {
-                iterCounter++;
-                if (ints[i] < ints[i - 1]) {
-                    int temp = ints[i];
-                    ints[i] = ints[i - 1];
-                    ints[i - 1] = temp;
-
-                    needIteration = true;
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(ints));
-        System.out.println("counter: " + iterCounter);
 
     }
 }
