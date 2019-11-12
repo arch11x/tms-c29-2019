fddd1c0826cdac47fafc50bb35946b8473e91335
alexfomin <rezviybelorus@gmail.com>
2019-11-12 09:17:19 +0300

    lec_11

--- src/tms/c29/TestMain.java
+++ src/tms/c29/TestMain.java
@@ -4,32 +4,16 @@ import tms.c29.lec_8.classwork.point_4.MyCustomException;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class TestMain {
     private static final int MAX_ARRAY_SIZE = 10;
 
     public static void main(String[] args) throws MyCustomException {
-        Boolean flag = true; // я думаю здесь можно обойтись примитивом boolean
-        while (flag) {
-            System.out.println("Please enter array dimension: ");
-            Scanner scanner = new Scanner(System.in);
-            int size = scanner.nextInt();
-            try {
-                // я бы предложил тебе проверку числа вынести в отдельный метод из которого выбрасывать исключение и затем создавать массив в main()
-                if (size > 0 && size < 10) {
-                    int[] array = new int[size];
-                    for (int i = 0; i < size; i++) {
-                        array[i] = (int) (Math.random() * 10);
-                    }
-                    System.out.println(Arrays.toString(array));
-                } else {
-                    throw new IndexOutOfBoundsException("Error in array dimension");
-                }
-                flag=false;
-            } catch (IndexOutOfBoundsException e) {
-//                throw new MyCustomException("Error in array dimension");
-            }
-        }
-    }//я не разобралась,что мне надо добавить,чтобы после Exception просило ввести еще раз.
+        HashSet<Object> objects = new HashSet<>();
+        objects.add(null);
+        objects.add(null);
+        System.out.println(objects);
+    }
 }
